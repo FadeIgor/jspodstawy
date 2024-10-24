e848ea59607ab874eed2fa6a3992a910505bcddb
IW <122602491+FadeIgor@users.noreply.github.com>
2024-10-24 10:57:24 +0200

    das

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ document.write("tak")
 const num1 = parseInt(prompt("wprowadz 1 liczbe"))
 const num2 = parseInt(prompt("wprowadz 2 liczbe"))
 
-document.write("wynik: "+ (num1+num2))
+if(isNaN(num1)){
+    document.write("to nie jest liczba")
+}
+else if(isNaN(num2)){
+    document.write("to nie jest liczba")
+}
+else document.write("wynik: "+ (num1+num2));
